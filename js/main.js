19f2560743877f5fcbca19c9f71ac8cc1f00381a
Mariana V <marianvj17@gmail.com>
2022-08-01 13:37:59 -0500

    JS

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+//Una alerta es una ventana de dialogo que muestra un mensaje 
+alert("Hola, gracias por visitar mi Portafolio");
+
+//Pedimos informacion de ususario con un prompt, la guardamos en una variable llamada nombre para despues mostrarla
+nombre = prompt ("¿Cómo te llamas?");
+
+//Usamos una console.log para mostar lo que se guarda en la variable llamada nombre
+console.log(nombre);
+
+
+alert("Ten un buen día");
